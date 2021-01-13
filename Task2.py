fd3878b5caa8cc6cd5602be93229fef8a7bf7333
Alexandra <Achichaj@yandex.ru>
2021-01-13 23:18:04 +0300

    updated Task2.py

--- Task2.py
+++ Task2.py
@@ -1,12 +1,12 @@
 time = int(input("Введите время в секундах: "))
 hour = time // 3600
-minute = (time - hour * 3600) // 60
-minute_second = (time - hour * 3600)
-second = minute_second - minute * 60
+th = time - hour * 3600
+minute = (th) // 60
+second = th - minute * 60
 if hour < 10:
-    hour = "0"+str(hour)
+    hour = "0" + str(hour)
 if minute < 10:
-    minute = "0"+str(minute)
+    minute = "0" + str(minute)
 if second < 10:
-    second = "0"+str(second)
+    second = "0" + str(second)
 print(f"{hour}:{minute}:{second}")
